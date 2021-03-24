c9ecdac04dc9610a5b4cb457d57bd23c5d1f0d97
Christian Thieme <christianthieme@u.boisestate.edu>
2021-03-24 12:21:55 -0600

    Updated with working graphs
    Updated with working graphs

--- Exploring NYC Tree Census with Dash and Plotly/Tree Census.py
+++ Exploring NYC Tree Census with Dash and Plotly/Tree Census.py
@@ -15,36 +15,43 @@ tree_health_by_boro_url = 'https://data.cityofnewyork.us/resource/uvpi-gqnh.json
 df = pd.read_json(tree_health_by_boro_url)
 df['spc_common'] = df['spc_common'].str.title()
 
-proportion = df.groupby('health')
+#proportion = df.groupby('health')
+
+# starting_graph = filtered_df = df[(df['boroname']=='Brooklyn') & (df['spc_common']=='American Beech')]
+# group_starting_graph = starting_graph.groupby('health').sum()
+
 
 app = dash.Dash()
 
 
-boro_options = []
-for boro in df['boroname'].unique():
-    boro_options.append({'label':boro, 'value':boro})
-
-species_options = []
-for spec in df['spc_common'].unique():
-    species_options.append({'label':spec, 'value':spec})
-
-
 app.layout = html.Div([
     html.Div([
-        dcc.Dropdown(id = 'boro', options = boro_options,  multi=True, value = 'Brooklyn')],
+        dcc.Dropdown(id = 'boro', options = [{'label':i, 'value':i} for i in df['boroname'].unique()], value = 'Brooklyn')],
         style = {'width':'48%', 'display':'inline-block'}), 
+    html.H1(id = 'h1-out'),
     html.Div([
-        dcc.Dropdown(id = 'species', options = species_options,  multi=True, value = 'American Beech')
+        dcc.Dropdown(id = 'species', options = [{'label':i, 'value':i} for i in df['spc_common'].unique()], value = 'American Beech')
         ], style = {'width':'48%', 'display':'inline-block'}),
-    html.Div(
-        [dcc.Graph(id = 'graphic')], style = {'padding':10})
-])
+    html.H1(id = 'h1-out2'),
+    dcc.Graph(id = 'feature-graphic')
+    ])
 
-@app.callback(Output('graphic', 'figure'), [Input('boro', 'value'), Input('species', 'value')])
+
+
+@app.callback(Output('feature-graphic', 'figure'), [Input('boro', 'value'), Input('species', 'value')])
 def update_graph(val_boro, val_species):
-    filtered_df = df[(df['boroname']==val_boro) & (df['spc_common']==val_species)]
-    grouped_df = filtered_df.groupby('health').sum()
-    return px.bar(grouped_df, x = grouped_df.index, y = 'count_tree_id')
+        filtered_df = df[(df['boroname']==val_boro) & (df['spc_common']==val_species)]
+        grouped_df = filtered_df.groupby('health').sum()
+        return px.bar(grouped_df, x = grouped_df.index, y = 'count_tree_id')
+
+
+@app.callback(Output('h1-out', 'children'), [Input('boro', 'value')])
+def ouput(borrows):
+    return "{} were selected".format(borrows)
+
+@app.callback(Output('h1-out2', 'children'), [Input('species', 'value')])
+def ouput_2(species):
+    return "{} were selected".format(species)
 
 if __name__ == '__main__':
     app.run_server()
