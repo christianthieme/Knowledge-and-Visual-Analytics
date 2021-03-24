7ed44eb095d077cfee33fc333ccb9dbed15cd670
Christian Thieme <christianthieme@u.boisestate.edu>
2021-03-23 23:40:22 -0600

    added dropdown multi-selector lists
    added dropdown multi-selector lists

--- Exploring NYC Tree Census with Dash and Plotly/Tree Census.py
+++ Exploring NYC Tree Census with Dash and Plotly/Tree Census.py
@@ -12,6 +12,31 @@ tree_health_by_boro_url = 'https://data.cityofnewyork.us/resource/uvpi-gqnh.json
         '&$limit=5000'
 
 df = pd.read_json(tree_health_by_boro_url)
+df['spc_common'] = df['spc_common'].str.title()
+
+proportion = df.groupby('health')
 
 app = dash.Dash()
 
+
+boro_options = []
+for boro in df['boroname'].unique():
+    boro_options.append({'label':boro, 'value':boro})
+
+species_options = []
+for spec in df['spc_common'].unique():
+    species_options.append({'label':spec, 'value':spec})
+
+
+app.layout = html.Div([
+    html.Div([
+    dcc.Dropdown(id = 'boro', options = boro_options,  multi=True, value = 'Brooklyn')],
+    style = {'width':'48%', 'display':'inline-block'}), 
+    html.Div([
+    dcc.Dropdown(id = 'species', options = species_options,  multi=True, value = 'Black Cherry')
+    ], style = {'width':'48%', 'display':'inline-block'})
+])
+
+
+if __name__ == '__main__':
+    app.run_server()
