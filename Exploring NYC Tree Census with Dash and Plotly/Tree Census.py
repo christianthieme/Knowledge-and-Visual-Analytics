55acd3a0a2d7b689e4c4ab24f4ae69a7c6cf221d
Christian Thieme <christianthieme@u.boisestate.edu>
2021-03-25 15:26:20 -0600

    Pushing final version
    Pushing final version

--- Exploring NYC Tree Census with Dash and Plotly/Tree Census.py
+++ Exploring NYC Tree Census with Dash and Plotly/Tree Census.py
@@ -50,13 +50,15 @@ app.layout = html.Div([
     html.Div([dcc.Markdown('''
     ## Background
 
-    This data was gathered as part of the New York City tree census. Data was collected for every tree within each of the five boroughs and includes data about location, species, and health of the tree. Additional information about this data can be found [here](https://data.cityofnewyork.us/Environment/2015-Street-Tree-Census-Tree-Data/uvpi-gqnh). This app makes a call to the NYC Open Data API to pull it's data. 
-    ''')],style = {'width':'45%'}),
+    This data was gathered as part of the New York City tree census. Data was collected for every tree within each of the five boroughs and includes data about location, species, and health of each tree. Additional information about this data can be found [here](https://data.cityofnewyork.us/Environment/2015-Street-Tree-Census-Tree-Data/uvpi-gqnh). This app makes a call to the NYC Open Data API to pull it's data. 
+    ''')]),
+
+    html.Hr(),
 
     html.H2("New York City Tree Population"), 
 
     html.Div([
-        html.H4("View Top __ Species by Count:"),
+        html.H4("How many species to display?:"),
         dcc.Input(id = 'top_x', type = 'number', value = 45),
         dcc.Graph(id = 'all_bar-graphic')],  style = {'width':'96%'}),
 
@@ -66,13 +68,19 @@ app.layout = html.Div([
         dcc.Graph(id = 'sunburst-graphic', figure = px.sunburst(proportions, path = ['health','boroname'], values = 'percent'))],  style = {'width':'48%','display':'inline-block'}),
 
         html.Div([
-        html.H4("Steward Project Statistics:"),
+        html.H4("Steward Project Statistics - Do Stewards Make a Difference in Tree Health Overall?:"),
         html.P("The below chart helps to interpret if the Steward Project overall is beneficial. We do not see large percentage differences between the health category of the trees and whether they have a steward or not."),
-        dcc.Graph(id = 'grouped-graphic', figure = px.bar(steward_percent_df, x = 'health', y = 'percent', color = 'steward', barmode = 'group', text = round(steward_percent_df['percent'],2)))],  style = {'padding-left': '30px','display':'inline-block','width':'48%'}),
+        dcc.Graph(id = 'grouped-graphic', figure = px.bar(steward_percent_df, x = 'health', y = 'percent', color = 'steward', barmode = 'group', text = round(steward_percent_df['percent'],2),
+        labels={
+                 "percent": "Percent", 
+                 "steward": "Steward",
+                 "health": "Tree Health"
+               }))],  style = {'padding-left': '30px','display':'inline-block','width':'48%'}),
 
+    html.Hr(),
     html.Div([
         html.H2("Tree Health Drill Down"),
-        html.P("The selectors below allow you to look at all/selection of boroughs and dive into a single tree species")], style = {'width':'96%'}),
+        html.P("The selectors below allow you to look at boroughs and dive into the details of tree species.")], style = {'width':'96%'}),
 
     html.Div([
         html.H4("Borough Selector:"),
@@ -81,11 +89,12 @@ app.layout = html.Div([
    
     html.Div([
         html.H4("Species Selector:"),
-        dcc.Dropdown(id = 'species', options = [{'label':i, 'value':i} for i in df['spc_common'].unique()], value = 'American Beech')], 
-        style = {'width':'48%', 'display':'inline-block'}),
+        dcc.Dropdown(id = 'species', options = [{'label':i, 'value':i} for i in df['spc_common'].unique()], multi = True, value = ['American Beech', 'American Elm', 'American Hophornbeam'])], 
+        style = {'width':'48%', 'display':'inline-block', 'padding-left': '15px'}),
     
     html.Div(dcc.Graph(id = 'feature-graphic'), style = {'display':'inline-block', 'width':'48%'}), 
-    html.Div(dcc.Graph(id = 'area-graphic'), style = {'display':'inline-block', 'width':'48%'})
+    html.Div(dcc.Graph(id = 'area-graphic'), style = {'display':'inline-block', 'width':'48%'}), 
+    html.Div(dcc.Graph(id = 'species_percent'), style = {'width':'96%'})
     ])
 
 @app.callback(Output('all_bar-graphic', 'figure'), [Input('top_x', 'value')])
@@ -96,7 +105,7 @@ def update_all_bar(top_x):
     fd['Species'] = np.where(fd['spc_common'].isin(top_list), fd['spc_common'], "Other")
     fd.sort_values('count_tree_id', ascending = False, inplace = True)
     fd = fd[fd['Species']!='Other']
-    return px.bar(fd, x = 'Species', y = 'count_tree_id',  color = 'boroname', title = "Top 45 Trees Species by Borough", 
+    return px.bar(fd, x = 'Species', y = 'count_tree_id',  color = 'boroname', title = "Top Tree Species by Borough", 
         labels={
                   "count_tree_id": "Count of Trees",
                   "Species": "",
@@ -106,7 +115,7 @@ def update_all_bar(top_x):
 
 @app.callback(Output('feature-graphic', 'figure'), [Input('boro', 'value'), Input('species', 'value')])
 def update_graph(val_boro, val_species):
-        filtered_df = df[(df['boroname'].isin(val_boro)) & (df['spc_common']==val_species)]
+        filtered_df = df[(df['boroname'].isin(val_boro)) & (df['spc_common'].isin(val_species))]
         grouped_df = filtered_df.groupby(['boroname','health']).sum().reset_index()
         return px.bar(grouped_df, x = 'health', y = 'count_tree_id',  color = 'boroname', title = "Count of Health of Trees by Borough", 
         labels={
@@ -117,16 +126,30 @@ def update_graph(val_boro, val_species):
 
 @app.callback(Output('area-graphic', 'figure'), [Input('boro', 'value'), Input('species', 'value')])
 def update_area_graph(val_boro, val_species):
-        filtered_df = df[(df['boroname'].isin(val_boro)) & (df['spc_common']==val_species)]
+        filtered_df = df[(df['boroname'].isin(val_boro)) & (df['spc_common'].isin(val_species))]
         grouped_df = filtered_df.groupby(['boroname','spc_common','health']).sum()
         grouped_again = grouped_df.groupby(level=0).apply(lambda x:1 * x / float(x.sum())).reset_index().rename(columns = {'count_tree_id':'percent'})
-        fig = px.treemap(grouped_again, path = [px.Constant('New York City'), 'boroname', 'health'], values = 'percent', color = 'boroname', hover_name = 'boroname', title = "Trees Health Proportion by Borough", 
+        fig = px.treemap(grouped_again, path = [px.Constant('New York City'), 'boroname', 'health'], values = 'percent', color = 'boroname', hover_name = 'boroname', title = "Tree Health Proportion by Borough", 
         labels={
                  "percent": "Percent:"
                })
         fig.update_traces( hovertemplate=None)
         fig.update_layout(hovermode="x")
         return fig
+
+@app.callback(Output('species_percent', 'figure'), [Input('boro', 'value'), Input('species', 'value')])
+def update_species_percent_graph(val_boro, val_species):
+    filtered_df = df[(df['boroname'].isin(val_boro)) & (df['spc_common'].isin(val_species))]
+    steward_df = filtered_df.groupby(['steward', 'health']).sum()
+    steward_percent_df = steward_df.groupby(level=0).apply(lambda x:1 * x / float(x.sum())).reset_index().rename(columns = {'count_tree_id':'percent'})
+    steward_percent_df['steward'] = np.where(steward_percent_df['steward'] =='None','0',steward_percent_df['steward'])
+    steward_percent_df.sort_values(by = 'steward', inplace = True)
+    return px.bar(steward_percent_df, x = 'health', y = 'percent', color = 'steward', barmode = 'group', text = round(steward_percent_df['percent'],2), title = "Steward Project Comparative Percentages - Do Stewards Make a Difference in Tree Health for the Selected Borough(s) and Species?", 
+        labels={
+                 "percent": "Percent", 
+                 "steward": "Steward",
+                 "health": "Tree Health"
+               })
 
 
 @app.callback(Output('h1-out', 'children'), [Input('boro', 'value')])
@@ -136,6 +159,7 @@ def ouput(borrows):
 @app.callback(Output('h1-out2', 'children'), [Input('species', 'value')])
 def ouput_2(species):
     return "{} were selected".format(species)
+
 
 if __name__ == '__main__':
     app.run_server()
